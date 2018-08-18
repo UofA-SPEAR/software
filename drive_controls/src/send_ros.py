d207deeff882c3cfca433392f22b3299a34e06a5
Jacob Reckhard <jacobreckhard@gmail.com>
2018-08-17 20:20:30 -0600

    drive should be working now

--- drive_controls/src/send_ros.py
+++ drive_controls/src/send_ros.py
@@ -5,7 +5,7 @@ from drive_system.msg import drive_cmd
 class Driver():
     def __init__(self):
         rospy.init_node("drive_control")
-        self.pub = rospy.Publisher("/send_drive", drive_cmd, queue_size=10)
+        self.pub = rospy.Publisher("/drive", drive_cmd, queue_size=10)
         rospy.loginfo("Started driver node")
 
     def send_cmd(self, left, right):
