536fc47bd8709e4e48a669ab43ff900a9c7f9465
rasdasdasdgoa <rado07@shaw.ca>
2018-09-23 09:15:17 -0600

    Completed ik function which uses wrist position. Outlined function which uses end effector position and wrist orientation.

--- arm-ik/arm-ik.cpp
+++ arm-ik/arm-ik.cpp
@@ -1,13 +1,55 @@
 //Placeholder values, although actual values shouldn't matter as long as the ratios are maintained (currently they are not)
-float BICEP_LENGTH = 1f;
-float FOREARM_LENGTH = 1f;
-float WRIST_LENGTH = 1f;
+const float BICEP_LENGTH = 1f;
+const float FOREARM_LENGTH = 1f;
+const float WRIST_LENGTH = 1f;
+
+//float SHOULDER_OFFSET = 1f;  Should offset doesn't matter, move the coordinate system up by this amount and the result will be the same
+const float ELBOW_OFFSET = 1f;
+
+const float ADJUSTED_SHOULDER_PITCH = atan2(ELBOW_OFFSET, BICEP_LENGTH);
 
 /*
- * Given the position of the wrist, calculate the motor angles of the two arms
+ * Calculate angles of the two inner arms using wrist position
  */
-float[] PosToAngArmsNoWrist(int x, int y, int z){
-    //float END_DISTANCE_2D = hypot(x, y); //Needs C++ 11
-    float shoulderYaw = atan2(y, x);
-    float elbowPitch = acos(( pow(BICEP_LENGTH, 2) + pow(FOREARM_LENGTH, 2) - pow(x, 2) - pow(y, 2) ) / (2*BICEP_LENGTH*FOREARM_LENGTH)) 
+float* PosToAngArmsNoWrist(int x, int y, int z){
+    //Normalize x, y, z, coordinates 
+    float normX = (float)x/100f;
+    float normY = (float)y/100f;
+    float normZ = (float)z/100f;
+
+    float xy3DReach = hypot(normX, normY); //Needs C++ 11
+    float shoulderYaw = atan2(normY, normX);
+
+    float elbowPitch = acos(
+        (pow(BICEP_LENGTH, 2) + pow(FOREARM_LENGTH, 2) - pow(xy3DReach, 2))
+	/ (2*BICEP_LENGTH*FOREARM_LENGTH));
+
+    float xy2DReach = hypot(xy3DReach, normZ);
+    float shoulderPitch = acos(
+        (pow(BICEP_LENGTH, 2) + pow(xy2DReach, 2) - pow(FOREARM_LENGTH, 2))
+	/ (2*BICEP_LENGTH*xy2DReach)) - atan2(normZ, xy3DReach);
+
+    static float angles[3] = {shoulderYaw,
+	    shoulderPitch + ADJUSTED_SHOULDER_PITCH, elbowPitch};
+    return angles;
+}
+
+/*
+ * Calculate angles of arms using tip of wrist position and wrist orientation
+ */
+float* PosToAngAllArms(int x, int y, int z, float alpha){
+    //Normalize x, y, z coordinates
+    float normX = (float)x/100f;
+    float normY = (float)y/100f;
+    float normZ = (float)z/100f;
+
+    float x2DReach = sqrt(pow(x, 2) + pow(y, 2));
+    float xy2DReachSquare = pow(x2DReach, 2) + pow(z, 2);
+    float xyReachWrist = sqrt(xy2DReachSquare + pow(WRIST_LENGTH, 2)
+        - (2*WRIST_LENGTH*(x2DReach*cos(alpha) + normZ*sin(alpha))));
+
+    //Now use same calculations as above with the calculated xyReachWrist
+    
+    static float angles[3];
+    return angles;
 }
