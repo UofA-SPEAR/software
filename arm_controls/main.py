3b6a26edea077385f6aeeb6962a4a23536cc3153
Lyndeno <lyndeno@gmail.com>
2018-09-23 11:10:37 -0600

    Added x,y,z sliders

--- arm_controls/main.py
+++ arm_controls/main.py
@@ -1,15 +1,66 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.label import Widget
-from kivy.uix.slider import Slider
 
-s = Slider(min=-100, max=100, value=25)
+from kivy.uix.gridlayout import GridLayout
+
+from kivy.uix.label import Widget
+from kivy.uix.label import Label
+from kivy.uix.slider import Slider
+from kivy.properties import NumericProperty
+
+
+class WidgetContainer(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(WidgetContainer, self).__init__(**kwargs)
+
+        self.cols = 4
+
+        self.slide_x = Slider(min=-100, max=100, value=0)
+        self.slide_y = Slider(min=-100, max=100, value=0, orientation='vertical')
+        self.slide_z = Slider(min=-100, max=100, value=0, orientation='vertical')
+
+        self.add_widget(Label(text='X Position'))
+        self.add_widget(self.slide_x)
+
+        self.add_widget(Label(text="X Value"))
+        self.ValueX = Label(text='0')
+        self.add_widget(self.ValueX)
+
+        self.add_widget(Label(text='Y Position'))
+        self.add_widget(self.slide_y)
+
+        self.add_widget(Label(text="Y Value"))
+        self.ValueY = Label(text='0')
+        self.add_widget(self.ValueY)
+
+        self.add_widget(Label(text='Z Position'))
+        self.add_widget(self.slide_z)
+
+        self.add_widget(Label(text="Z Value"))
+        self.ValueZ = Label(text='0')
+        self.add_widget(self.ValueZ)
+
+        self.slide_x.bind(value=self.on_valueX)
+        self.slide_y.bind(value=self.on_valueY)
+        self.slide_z.bind(value=self.on_valueZ)
+
+    def on_valueX(self, instance, inValue):
+        self.ValueX.text = "%d"%inValue
+
+    def on_valueY(self, instance, inValue):
+        self.ValueY.text = "%d"%inValue
+    
+    def on_valueZ(self, instance, inValue):
+        self.ValueZ.text = "%d"%inValue
 
 class ArmInterface(App):
 
     def build(self):
-        return s
+        widgetcontainer = WidgetContainer()
+
+        return widgetcontainer
 
 
 
