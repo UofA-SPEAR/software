3a8e015c58cd47f421701cd2fe48e238ca6485b1
Lyndeno <lyndeno@gmail.com>
2018-09-24 19:40:13 -0600

    Rough layout including all required controls

--- arm_controls/main.py
+++ arm_controls/main.py
@@ -4,6 +4,7 @@ from kivy.app import App
 
 from kivy.uix.gridlayout import GridLayout
 
+from kivy.core.window import Window
 from kivy.uix.label import Widget
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -13,7 +14,7 @@ from kivy.properties import NumericProperty, ListProperty
 # We will be using the knob class from kivy garden to add a control knob to our app.
 # Use 'pip install kivy-garden' and then 'garden install knob' to install the knob class
 # See here: https://github.com/kivy-garden/garden.knob
-# from kivy.garden.knob import Knob
+from kivy.garden.knob import Knob
 
 
 
@@ -21,6 +22,8 @@ class WidgetContainer(GridLayout):
     pass
 # Define our app
 class PanelApp(App):
+    vSlideWidth = NumericProperty(50)
+    hGridHeight = NumericProperty(Window.size[1]/6)
 
     def build(self):
         self.title = 'SPEAR Arm Control Panel'
