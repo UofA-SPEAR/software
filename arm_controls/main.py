93fbca18afc76ade9bb18ceb809fd3477550f4eb
Lyndeno <lyndeno@gmail.com>
2018-09-30 11:33:05 -0600

    Re-did text boxes for vertical sliders

--- arm_controls/main.py
+++ arm_controls/main.py
@@ -7,9 +7,10 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.uix.label import Widget
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.slider import Slider
-from kivy.properties import NumericProperty, ListProperty
+from kivy.properties import NumericProperty, ListProperty, ObjectProperty, StringProperty
 
 # We will be using the knob class from kivy garden to add a control knob to our app.
 # Use 'pip install kivy-garden' and then 'garden install knob' to install the knob class
@@ -22,8 +23,10 @@ class WidgetContainer(GridLayout):
     pass
 # Define our app
 class PanelApp(App):
-    vSlideWidth = NumericProperty(50)
+    vSlideWidth = NumericProperty(60)
     hGridHeight = NumericProperty(Window.size[1]/6)
+    labelHeight = NumericProperty(20)
+    tinputHeight = NumericProperty(30)
 
     def build(self):
         self.title = 'SPEAR Arm Control Panel'
@@ -31,6 +34,10 @@ class PanelApp(App):
         widgetcontainer = WidgetContainer()
 
         return widgetcontainer
+
+    def displayInput(self, mySlideValue):
+        return str(round(mySlideValue, 2))
+
     def checkInput(self, myText):
         try:
             newNum = float(myText)
@@ -38,6 +45,10 @@ class PanelApp(App):
             newNum = 0
         return newNum
 
+class inputPopup(Popup):
+    inputValue = StringProperty(None)
+
+    pass
 
 
 # Run app
