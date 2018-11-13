868ff063355920c8f192f6e1353a0d6a82a7fe92
Lyndeno <lyndeno@gmail.com>
2018-11-12 17:06:35 -0800

    Code cleanup

--- arm_controls/ros_station.py
+++ arm_controls/ros_station.py
@@ -3,29 +3,7 @@
 import rospy
 import threading
 from threading import Thread
-from std_msgs.msg import String
-
-
-''' Lyndon's old garbage code
-def __init__(self, interval=1):
-    self.interval = interval
-
-    thread = threading.Thread(target=self.listener, args=())
-    thread.daemon = True
-    thread.start()
-
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + data.data)
-
-def listener():
-    armTopic = "arm_control"
-    print 'ROS Subscribe node subscribed to topic: ' + armTopic # check to see that thread is running
-    rospy.init_node('panel_node', anonymous=True)
-
-    rospy.Subscriber(armTopic, String, callback)
-
-    # rospy.spin()
-'''
+#from joyinput.msg import input_axes, input_buttons # Messages need to be compiled
 
 # from Rover1 Code
 class SpinROS(threading.Thread):
@@ -36,10 +14,12 @@ class SpinROS(threading.Thread):
         rospy.spin()
 
 def ros_init():
-    global arm_publisher
+    global joy_publisher
+    global button_publisher
 
     rospy.init_node('arm_controller')
     rospy.loginfo("Initializing the arm input node")
     
     # Extra configuration needed
-    # arm_publisher = rospy.Publisher('/user_arm_controls', input_arm, queue_size=50)
+    #joy_publisher = rospy.Publisher('/user_arm_controls', input_axes, queue_size=50)
+    #button_publisher = rospy.Publisher('/user_arm_controls', input_buttons, queue_size=50)
