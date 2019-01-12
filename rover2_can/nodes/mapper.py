877f7342bfc61cddefa4e1c337f9c0731fe59d9d
jo12bar <johann.b@telus.net>
2019-01-11 19:22:55 -0700

    Support "splitting" a ros message into multiple UAVCAN message.
    This will probably be useful for things like the drive system, where
    we're going to be receiving one ros message and we have to send out
    multiple UAVCAN messages (one to each wheel). By doing it this way, we
    avoid the memory overhead of using multiple ros subscribers.

--- rover2_can/nodes/mapper.py
+++ rover2_can/nodes/mapper.py
@@ -17,8 +17,17 @@ def main():
     # TODO: Map left & right from the ros message to the wheel number & speed
     #       in the UAVCAN message somehow...
     map_ros_to_can(drive_cmd, "/drive", "spear.drive.DriveCommand", {
-        "wheel": lambda data: 2,
+        "wheel": lambda data: 0,
         "speed": lambda data: 783
+    }, {
+        "wheel": lambda data: 1,
+        "speed": lambda data: 42
+    }, {
+        "wheel": lambda data: 2,
+        "speed": lambda data: 123
+    }, {
+        "wheel": lambda data: 3,
+        "speed": lambda data: 987
     })
 
     rospy.spin()
--- rover2_can/src/rover2_can/util.py
+++ rover2_can/src/rover2_can/util.py
@@ -5,7 +5,7 @@ from functools import partial
 import rospy
 
 
-def map_ros_to_can(ros_msg, ros_topic, can_msg, mapping):
+def map_ros_to_can(ros_msg, ros_topic, can_msg, *mappings):
     """
     Subscribes to the specified ros_topic & message type, maps ros message data
     from ros's format to UAVCAN's format using mapping functions specified in
@@ -24,7 +24,7 @@ def map_ros_to_can(ros_msg, ros_topic, can_msg, mapping):
         })
 
     Parameters:
-    
+
         - `ros_msg`: The rospy-generated datatype for the ros message you want
         to subscribe to.
 
@@ -34,28 +34,40 @@ def map_ros_to_can(ros_msg, ros_topic, can_msg, mapping):
         - `can_msg`: A string. The name of the UAVCAN message you want to
         publish, in the same format as described in UAVCAN's docs.
 
-        - `mapping`: A dictionary. Keys are the UAVCAN message parameters.
+        - `*mappings`: Dictionaries. Keys are the UAVCAN message parameters.
         Values are functions that take one value (the ros message data) and
         return a single value, which gets assigned to that UAVCAN message
         parameter.
+        If more than one mapping is passed, like with:
+
+            map_ros_to_can(RosMsg, "/rostopic", "uavcan.message", { ... mapping 1 ... }, { ... mapping 2 ... }, ...)
+
+        ...then each mapping will receive the same RosMsg data, and each one
+        will publish a unique message to "uavcan.message". This is to deal
+        with cases where you might want to "split up" a single ros message into
+        multiple UAVCAN messages (of the same type), without wanting to create
+        several ros message subscribers (therefore saving memory).
     """
 
-    def cb(can_pub, can_msg, mapping, data):
+    def cb(can_pub, can_msg, mappings, data):
+        # To reduce t
+
         # Loop over the mappings & call the functions within to translate data from
         # the ros message to data on the can message.
         #
         # The keys of the mapping dictionary are used as the property name on the can message.
-        for can_msg_param, ros_msg_mapper in mapping.iteritems():
-            setattr(can_msg, can_msg_param, ros_msg_mapper(data))
-
-        can_pub.publish(can_msg)
+        for mapping in mappings:
+            for can_msg_param, ros_msg_mapper in mapping.iteritems():
+                setattr(can_msg, can_msg_param, ros_msg_mapper(data))
+            can_pub.publish(can_msg)
 
     rospy.loginfo(
         "Mapping ros message {} on topic {} to UAVCAN message {}.".format(
             ros_msg._type, ros_topic, can_msg))
 
     _can_msg = canros.Message(can_msg)
+
     rospy.Subscriber(
         ros_topic, ros_msg,
         partial(cb, _can_msg.Publisher(queue_size=10), _can_msg.Type(),
-                mapping))
+                mappings))
