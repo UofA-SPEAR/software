e26b53a1c80cd69df25bd86c00d4ed65738cd63e
David Lenfesty <lenfesty@ualberta.ca>
2019-01-13 19:27:45 -0700

    Made subscribers and publishers global to use in callback.

--- hardware_interface/src/interface.cpp
+++ hardware_interface/src/interface.cpp
@@ -1,20 +1,35 @@
 #include "ros/ros.h"
 #include "drive_controls/drive_cmd.h"
 
-#include <sstream>o
-void driveCallback(const drive_controls::drive_cmd::ConstPtr& msg) {
+#include <sstream>
+
+auto wheel_out;
+
+ros::NodeHandle node;
+ros::Subscriber drive_sub;
+ros::Publisher wheel_pub;
+
+/** @brief Callback for receiving messages from drive controller.
+ *
+ */
+void driveControlsCallback(const drive_controls::drive_cmd::ConstPtr& msg) {
     ROS_INFO("Left: %d -- Right: %d", msg->left, msg->right);
+
+
 }
 
 
 int main(int argc, char **argv) {
 
+    // Initialise ROS node
 	ros::init(argc, argv, "hardware_interface");
 
-    ros::NodeHandle node;
+    // Initialise publishers and subscribers.
+    drive = node.subscribe("/drive", 100, driveControlsCallback);
+    wheel_pub = node.advertise<drive_controls::drive_cmd>("/hw_interface/wheels", 100);
 
-    ros::Subscriber drive = node.subscribe("/drive", 100, driveCallback);
 
+    // Infinite Loop
     ros::spin();
 
     return 0;
