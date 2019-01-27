e08ac255573b95027261824ae74692203bbaa787
Ryan Shukla <4721319+rpshukla@users.noreply.github.com>
2019-01-27 10:30:05 -0700

    Make ros package

---
+++ unpack.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+###### Get script location
+# https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+###### Setup ros path
+cd ~
+mkdir -p ~/ros/src
+cd ~/ros
+catkin_make
+echo "source ~/ros/devel/setup.bash" >> ~/.bashrc
+source ~/ros/devel/setup.bash
+
+###### Link the "simulator" package to the ros workspace
+cd ~/ros/src
+ln -s $DIR/simulator
+
+###### Build everything
+cd ~/ros
+catkin_make
