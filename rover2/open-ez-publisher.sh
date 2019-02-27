8a424a23a9983284977b16fab916e2bba62d063b
David Lenfesty <lenfesty@ualberta.ca>
2019-02-27 16:42:37 -0700

    moved README into rover2

--- rover2/open-ez-publisher.sh
+++ rover2/open-ez-publisher.sh
@@ -1,4 +1,4 @@
 # Must apt install ros-kinetic-rqt-ez-publisher
 # roscore must be running
 # a node must be subscribed to the topic you're trying to publish to
-rosrun rqt_ez_publisher rqt_ez_publisher --slider-file config/arm_control.yaml
+rosrun rqt_ez_publisher rqt_ez_publisher --slider-file config/new.yaml
