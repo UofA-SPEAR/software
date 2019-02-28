b6e96ce7514fddf3cea4312eff96657b7fce2ea7
David Lenfesty <lenfesty@ualberta.ca>
2019-02-27 17:41:27 -0700

    Started the great moving

--- spear_rover/setup.py
+++ spear_rover/setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['rover2_can'], package_dir={'': 'src'})
+    packages=['spear_rover'], package_dir={'': 'src'})
 
 setup(**setup_args)
