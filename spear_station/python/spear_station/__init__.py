f27a95df32a5021ac6b2d8765b617da396b5a198
David Lenfesty <lenfesty@ualberta.ca>
2019-03-10 16:57:01 -0600

    Fixed formatting issue

--- spear_station/python/spear_station/__init__.py
+++ spear_station/python/spear_station/__init__.py
@@ -4,3 +4,4 @@ File to export stuff to make it easier to use and clean up the directory structu
 
 from ros_station import SpinROS, ros_init, joyData, publish
 from send_ros import SpinROS, ros_init, joyData, publish, Driver
+
