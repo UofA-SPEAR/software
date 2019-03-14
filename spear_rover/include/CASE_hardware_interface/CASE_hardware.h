5ec5a0807f2be8ab7fad6e3f15866de8a4dbf204
David Lenfesty <lenfesty@ualberta.ca>
2019-03-13 21:18:54 -0600

    Fixed C++ formatting

--- spear_rover/include/CASE_hardware_interface/CASE_hardware.h
+++ spear_rover/include/CASE_hardware_interface/CASE_hardware.h
@@ -5,52 +5,53 @@
 #ifndef ROS_CONTROL__CASE_HARDWARE_H
 #define ROS_CONTROL__CASE_HARDWARE_H
 
-#include <hardware_interface/joint_state_interface.h>
+#include <controller_manager/controller_manager.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <joint_limits_interface/joint_limits.h>
 #include <joint_limits_interface/joint_limits_interface.h>
 #include <joint_limits_interface/joint_limits_rosparam.h>
-#include <controller_manager/controller_manager.h>
-#include <boost/scoped_ptr.hpp>
 #include <ros/ros.h>
+#include <boost/scoped_ptr.hpp>
 
 namespace CASE_hardware_interface {
-    class CASEHardware : public hardware_interface::RobotHW {
-        protected:
-            // Interfaces
-            hardware_interface::JointStateInterface joint_state_interface_;
-            hardware_interface::VelocityJointInterface velocity_joint_interface_;
-            hardware_interface::PositionJointInterface position_joint_interface_;
-            hardware_interface::EffortJointInterface effort_joint_interface_;
+class CASEHardware : public hardware_interface::RobotHW {
+ protected:
+  // Interfaces
+  hardware_interface::JointStateInterface joint_state_interface_;
+  hardware_interface::VelocityJointInterface velocity_joint_interface_;
+  hardware_interface::PositionJointInterface position_joint_interface_;
+  hardware_interface::EffortJointInterface effort_joint_interface_;
 
-            joint_limits_interface::VelocityJointSaturationInterface velocity_joint_saturation_interface_;
-            joint_limits_interface::VelocityJointSoftLimitsInterface velocity_joint_limits_interface_;
+  joint_limits_interface::VelocityJointSaturationInterface
+      velocity_joint_saturation_interface_;
+  joint_limits_interface::VelocityJointSoftLimitsInterface
+      velocity_joint_limits_interface_;
 
-            // Custom or available transmissions
-            //
-            
-            // Shared memory
-            int num_joints_;
-            int joint_mode_; // position, velocity, or effort
-            std::vector<std::string> joint_names_;
-            std::vector<int> joint_types_;
-            std::vector<double> joint_velocity_;
-            std::vector<double> joint_velocity_command_;
-            std::vector<double> joint_lower_limits_;
-            std::vector<double> joint_upper_limits_;
-            std::vector<double> joint_effort_limits_;
-            // Required for joint state handle
-            std::vector<double> joint_position_;
-            std::vector<double> joint_position_command_;
-            std::vector<double> joint_effort_;
-            std::vector<double> joint_effort_command_;
+  // Custom or available transmissions
+  //
 
-            // Publisher for sending CAN commands
-            ros::Publisher wheel_pub;
-    }; // class
+  // Shared memory
+  int num_joints_;
+  int joint_mode_;  // position, velocity, or effort
+  std::vector<std::string> joint_names_;
+  std::vector<int> joint_types_;
+  std::vector<double> joint_velocity_;
+  std::vector<double> joint_velocity_command_;
+  std::vector<double> joint_lower_limits_;
+  std::vector<double> joint_upper_limits_;
+  std::vector<double> joint_effort_limits_;
+  // Required for joint state handle
+  std::vector<double> joint_position_;
+  std::vector<double> joint_position_command_;
+  std::vector<double> joint_effort_;
+  std::vector<double> joint_effort_command_;
 
-}; // namespace
+  // Publisher for sending CAN commands
+  ros::Publisher wheel_pub;
+};  // class
 
+};  // namespace
 
 #endif
--- spear_rover/src/CASE_hardware_interface.cpp
+++ spear_rover/src/CASE_hardware_interface.cpp
@@ -1,12 +1,12 @@
 // Actually implement hardware interface
 //
 
-#include <sstream>
 #include <CASE_hardware_interface/CASE_hardware_interface.h>
-#include <joint_limits_interface/joint_limits_interface.h>
 #include <joint_limits_interface/joint_limits.h>
-#include <joint_limits_interface/joint_limits_urdf.h>
+#include <joint_limits_interface/joint_limits_interface.h>
 #include <joint_limits_interface/joint_limits_rosparam.h>
+#include <joint_limits_interface/joint_limits_urdf.h>
+#include <sstream>
 
 // message includes
 #include <canros/uavcan__equipment__actuator__ArrayCommand.h>
@@ -18,88 +18,92 @@ using joint_limits_interface::SoftJointLimits;
 using joint_limits_interface::PositionJointSoftLimitsHandle;
 using joint_limits_interface::PositionJointSoftLimitsInterface;
 
-namespace CASE_hardware_interface
-{
-    CASEHardwareInterface::CASEHardwareInterface(ros::NodeHandle& nh) : nh_(nh) {
-        init();
-        controller_manager_.reset(new controller_manager::ControllerManager(this, nh_));
-        nh_.param("/CASE/hardware_interface/loop_hz", loop_hz_, 0.1);
-        ros::Duration update_freq = ros::Duration(1.0/loop_hz_);
-        non_realtime_loop_ = nh_.createTimer(update_freq, &CASEHardwareInterface::update, this);
+namespace CASE_hardware_interface {
+CASEHardwareInterface::CASEHardwareInterface(ros::NodeHandle& nh) : nh_(nh) {
+  init();
+  controller_manager_.reset(
+      new controller_manager::ControllerManager(this, nh_));
+  nh_.param("/CASE/hardware_interface/loop_hz", loop_hz_, 0.1);
+  ros::Duration update_freq = ros::Duration(1.0 / loop_hz_);
+  non_realtime_loop_ =
+      nh_.createTimer(update_freq, &CASEHardwareInterface::update, this);
 
-        // temporary topic to publish wheel stuff on
-        // should this actually publish straight to canros?
-        wheel_pub = nh_.advertise<uavcan__equipment__actuator__ArrayCommand>("/canros/msg/uavcan/equipment/actuator/ArrayCommand", 10);
-    }
+  // temporary topic to publish wheel stuff on
+  // should this actually publish straight to canros?
+  wheel_pub = nh_.advertise<uavcan__equipment__actuator__ArrayCommand>(
+      "/canros/msg/uavcan/equipment/actuator/ArrayCommand", 10);
+}
 
-    CASEHardwareInterface::~CASEHardwareInterface() {
+CASEHardwareInterface::~CASEHardwareInterface() {}
 
-    }
+// Returns 1 if initialization success
+bool CASEHardwareInterface::init() {
+  // Get joint names
+  nh_.getParam("/CASE/hardware_interface/joints", joint_names_);
+  num_joints_ = joint_names_.size();
 
-    // Returns 1 if initialization success
-    bool CASEHardwareInterface::init() {
-        // Get joint names
-        nh_.getParam("/CASE/hardware_interface/joints", joint_names_);
-        num_joints_ = joint_names_.size();
+  // Resize vectors
+  joint_velocity_.resize(num_joints_);
+  joint_velocity_command_.resize(num_joints_);
 
-        // Resize vectors
-        joint_velocity_.resize(num_joints_);
-        joint_velocity_command_.resize(num_joints_);
-        
-        // These are only here for the joint state handle
-        joint_position_.resize(num_joints_);
-        joint_position_command_.resize(num_joints_);
-        joint_effort_.resize(num_joints_);
-        joint_effort_command_.resize(num_joints_);
+  // These are only here for the joint state handle
+  joint_position_.resize(num_joints_);
+  joint_position_command_.resize(num_joints_);
+  joint_effort_.resize(num_joints_);
+  joint_effort_command_.resize(num_joints_);
 
-        // Initialize Controller 
-        // Need to 
-        for (int i = 0; i < num_joints_; ++i) {
-             // Create joint state interface
-            JointStateHandle jointStateHandle(joint_names_[i], &joint_position_[i], &joint_velocity_[i], &joint_effort_[i]);
-             joint_state_interface_.registerHandle(jointStateHandle);
+  // Initialize Controller
+  // Need to
+  for (int i = 0; i < num_joints_; ++i) {
+    // Create joint state interface
+    JointStateHandle jointStateHandle(joint_names_[i], &joint_position_[i],
+                                      &joint_velocity_[i], &joint_effort_[i]);
+    joint_state_interface_.registerHandle(jointStateHandle);
 
-            // Create joint velocity interface
-            JointHandle jointVelocityHandle(jointStateHandle, &joint_velocity_command_[i]);
-            velocity_joint_interface_.registerHandle(jointVelocityHandle);
-        }
+    // Create joint velocity interface
+    JointHandle jointVelocityHandle(jointStateHandle,
+                                    &joint_velocity_command_[i]);
+    velocity_joint_interface_.registerHandle(jointVelocityHandle);
+  }
 
-        registerInterface(&joint_state_interface_);
-        registerInterface(&velocity_joint_interface_);
+  registerInterface(&joint_state_interface_);
+  registerInterface(&velocity_joint_interface_);
 
-        // We also need to do any hardware init here (if we have any)
-    }
+  // We also need to do any hardware init here (if we have any)
+}
 
-    void CASEHardwareInterface::update(const ros::TimerEvent& e) {
-        elapsed_time_ = ros::Duration(e.current_real - e.last_real);
-        read();
-        controller_manager_->update(ros::Time::now(), elapsed_time_);
-        write(elapsed_time_);
-    }
+void CASEHardwareInterface::update(const ros::TimerEvent& e) {
+  elapsed_time_ = ros::Duration(e.current_real - e.last_real);
+  read();
+  controller_manager_->update(ros::Time::now(), elapsed_time_);
+  write(elapsed_time_);
+}
 
-    void CASEHardwareInterface::read() {
-        for (int i = 0; i < num_joints_; i++) {
-            // Read in all values from hardware
-        }
-    }
+void CASEHardwareInterface::read() {
+  for (int i = 0; i < num_joints_; i++) {
+    // Read in all values from hardware
+  }
+}
 
-    void CASEHardwareInterface::write(ros::Duration elapsed_time) {
-        // Not entirely sure how this is emplemented
-        velocityJointSoftLimitsInterface.enforceLimits(elapsed_time);
+void CASEHardwareInterface::write(ros::Duration elapsed_time) {
+  // Not entirely sure how this is emplemented
+  velocityJointSoftLimitsInterface.enforceLimits(elapsed_time);
 
-        // Write ArrayCommands to wheels
-        uavcan__equipment__actuator__ArrayCommand out_msg;
-        for (int i = 0; i < num_joints_; i++) {
-            uavcan__equipment__actuator__Command out_cmd;
+  // Write ArrayCommands to wheels
+  uavcan__equipment__actuator__ArrayCommand out_msg;
+  for (int i = 0; i < num_joints_; i++) {
+    uavcan__equipment__actuator__Command out_cmd;
 
-            out_cmd.actuator_id = i;
-            out_cmd.command_type = uavcan__equipment__actuator__Command::COMMAND_TYPE_SPEED;
-            // mapping a double to a float here but eh
-            out_cmd.command_value = velocity_joint_interface_.getHandle(joint_names_[i]).getCommand();
-            
-            out_msg.commands.push_back(out_cmd);
-        }
+    out_cmd.actuator_id = i;
+    out_cmd.command_type =
+        uavcan__equipment__actuator__Command::COMMAND_TYPE_SPEED;
+    // mapping a double to a float here but eh
+    out_cmd.command_value =
+        velocity_joint_interface_.getHandle(joint_names_[i]).getCommand();
 
-        wheel_pub.publish(out_msg);
-    }
+    out_msg.commands.push_back(out_cmd);
+  }
+
+  wheel_pub.publish(out_msg);
+}
 }
