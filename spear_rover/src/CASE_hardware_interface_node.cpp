884a36c20c963738c44aef2b47173f7e953707a5
jo12bar <johann.b@telus.net>
2019-03-27 18:33:01 -0600

    Potentially fix diff drive...?
    To be tested.
    
    Also screw threading.

--- spear_rover/src/CASE_hardware_interface_node.cpp
+++ spear_rover/src/CASE_hardware_interface_node.cpp
@@ -1,11 +1,10 @@
 #include <CASE_hardware_interface/CASE_hardware_interface.h>
 
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "CASE_hardware_interface");
-  ros::NodeHandle nh;
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-  CASE_hardware_interface::CASEHardwareInterface CASE(nh);
-  ros::spin();
-  return 0;
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "CASE_hardware_interface");
+    ros::NodeHandle nh;
+    CASE_hardware_interface::CASEHardwareInterface CASE(nh);
+    ros::spin();
+    return 0;
 }
