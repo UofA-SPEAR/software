0717736b894354780f5463a3dea254765b7e56a8
mdepp <mdepp@ualberta.ca>
2019-05-19 11:21:32 -0600

    State to move base to coordinate

--- spear_rover/python/planner.py
+++ spear_rover/python/planner.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python
-from smach import State, StateMachine, Sequence
-
+import actionlib
 import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion, Twist, Vector3
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from smach import State, StateMachine, Sequence
 from spear_msgs.msg import ArmAngles
+from std_msgs.msg import String, Header
 from time import sleep
+
+
+class MoveRoverToCoord(State):
+    def __init__(self):
+        State.__init__(self, outcomes=['ok', 'err'], input_keys=['coord'])
+        self.client = actionlib.SimpleActionClient('move_base_simple/goal', MoveBaseAction)
+
+    def execute(self, userdata):
+        rospy.loginfo('Waiting for move_base action server...')
+        self.client.wait_for_server()
+
+        header = Header(
+            frame_id='base_link',
+            stamp=rospy.Time.now(),
+        )
+        x, y = userdata.coord
+        pos = Point(x, y, 0)
+        orient = Quaternion(0, 0, 0, 1)
+        goal = MoveBaseGoal(target_pose=PoseStamped(
+            header=header,
+            pose=Pose(position=pos, orientation=orient),
+        ))
+        rospy.loginfo(goal)
+        self.client.send_goal(goal)
+        self.client.wait_for_result()
+
+        if self.client.get_result() == actionlib.GoalStatus.SUCCEEDED:
+            return 'ok'
+        else:
+            return 'err'
 
 
 class MoveRoverForward(State):
@@ -42,12 +73,13 @@ class TurnRoverClockwise(State):
 
 def main():
     rospy.init_node('mission_planner')
-    sq = Sequence(outcomes=['ok', 'err'], connector_outcome='ok')
+    sq = Sequence(outcomes=['ok', 'err'], connector_outcome='ok', input_keys=['coord'])
     with sq:
         Sequence.add('forward', MoveRoverForward())
+        Sequence.add('move-to-coord', MoveRoverToCoord())
         Sequence.add('turn', TurnRoverClockwise(), transitions={'ok': 'forward'})
 
-    sq.execute()
+    sq.execute({'coord': (1, 1)})
 
 
 if __name__ == '__main__':
