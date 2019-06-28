636af57f5615cadf6377945e1b78a37dac28b4de
mdepp <mdepp@ualberta.ca>
2019-06-27 20:41:43 -0600

    Fix planner

--- spear_rover/nodes/planner.py
+++ spear_rover/nodes/planner.py
@@ -12,23 +12,21 @@ from time import sleep
 class MoveRoverToCoord(State):
     def __init__(self):
         State.__init__(self, outcomes=['ok', 'err'], input_keys=['coord'])
-        self.client = actionlib.SimpleActionClient('move_base_simple/goal', MoveBaseAction)
+        self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
     def execute(self, userdata):
         rospy.loginfo('Waiting for move_base action server...')
         self.client.wait_for_server()
+        rospy.loginfo('Found.')
 
-        header = Header(
-            frame_id='base_link',
-            stamp=rospy.Time.now(),
-        )
         x, y = userdata.coord
-        pos = Point(x, y, 0)
-        orient = Quaternion(0, 0, 0, 1)
-        goal = MoveBaseGoal(target_pose=PoseStamped(
-            header=header,
-            pose=Pose(position=pos, orientation=orient),
-        ))
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        goal.target_pose.pose.orientation.w = 1.0
+
         rospy.loginfo(goal)
         self.client.send_goal(goal)
         self.client.wait_for_result()
@@ -39,47 +37,13 @@ class MoveRoverToCoord(State):
             return 'err'
 
 
-class MoveRoverForward(State):
-    def __init__(self):
-        State.__init__(self, outcomes=['ok', 'err'])
-        self.publisher = rospy.Publisher('/rover_diff_drive_controller/cmd_vel', Twist, queue_size=10)
-
-    def execute(self, userdata):
-        twist = Twist(
-            linear=Vector3(10, 0, 0),
-            angular=Vector3(0, 0, 0),
-        )
-        for _ in range(100):
-            self.publisher.publish(twist)
-            sleep(0.1)
-        return 'ok'
-
-
-class TurnRoverClockwise(State):
-    def __init__(self):
-        State.__init__(self, outcomes=['ok', 'err'])
-        self.publisher = rospy.Publisher('/rover_diff_drive_controller/cmd_vel', Twist, queue_size=10)
-
-    def execute(self, userdata):
-        twist = Twist(
-            linear=Vector3(0, 0, 0),
-            angular=Vector3(0, 0, -3.1415926535897 / 2),
-        )
-        for _ in range(100):
-            self.publisher.publish(twist)
-            sleep(0.1)
-        return 'ok'
-
-
 def main():
     rospy.init_node('mission_planner')
     sq = Sequence(outcomes=['ok', 'err'], connector_outcome='ok', input_keys=['coord'])
     with sq:
-        Sequence.add('forward', MoveRoverForward())
         Sequence.add('move-to-coord', MoveRoverToCoord())
-        Sequence.add('turn', TurnRoverClockwise(), transitions={'ok': 'forward'})
 
-    sq.execute({'coord': (1, 1)})
+    sq.execute({'coord': (1.0, 1.0)})
 
 
 if __name__ == '__main__':
