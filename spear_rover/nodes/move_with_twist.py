3b88eb7c2d31d52fcf8074ca4608e0f63062ed18
mdepp <mdepp@ualberta.ca>
2019-08-10 12:20:59 -0600

    Add a node for moving via twist messages

---
+++ spear_rover/nodes/move_with_twist.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import rospy
+from time import sleep, time
+from geometry_msgs.msg import Twist
+
+def main():
+    rospy.init_node('move_with_twist')
+    action = rospy.get_param('~action', 'forward')
+    duration = rospy.get_param('~duration', '5')
+    duration = float(duration)
+    frequency = 10
+
+    import sys
+    rospy.loginfo(sys.argv)
+
+    publisher = rospy.Publisher('/rover_diff_drive_controller/cmd_vel', Twist)
+
+    message = Twist()
+    if action == 'forward':
+        message.linear.x = 2
+    elif action == 'reverse':
+        message.linear.x = -2
+    elif action == 'left':
+        message.angular.z = 0.5
+    elif action == 'right':
+        message.angular.z = -0.5
+    
+    rospy.loginfo('Movement started: %s.' % str(message))
+    initial_time = time()
+    while True:
+        publisher.publish(message)
+        if (time() - initial_time) > duration:
+            break
+        sleep(1/frequency)
+
+    rospy.loginfo('Movement finished.')
+
+
+if __name__ == '__main__':
+    main()
