27fbd2c4f9830f908eeaf3fce02ff5964065a9e6
rpshukla <4721319+rpshukla@users.noreply.github.com>
2019-08-09 23:30:29 -0600

    Reset script works now

---
+++ reset_node.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+import uavcan
+
+def response_callback(event):
+    # do nothing
+    print("Received Callback!")
+
+
+node = uavcan.make_node("can0", node_id=120)
+
+node.request(uavcan.protocol.RestartNode.Request(), 20, response_callback)
+
+node.spin()
