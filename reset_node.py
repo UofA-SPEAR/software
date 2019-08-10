a13dace2cc54382b85f067248c6e85a922a18cb0
David Lenfesty <lenfesty@ualberta.ca>
2019-08-09 23:26:29 -0600

    Added stuff

--- reset_node.py
+++ reset_node.py
@@ -4,6 +4,7 @@ import uavcan
 
 def response_callback(event):
     # do nothing
+    print("Received Callback!")
 
 msg = uavcan.protocol.RestartNode(magic_number=uavcan.protocol.RestartNode.MAGIC_NUMBER)
 
@@ -11,3 +12,5 @@ msg = uavcan.protocol.RestartNode(magic_number=uavcan.protocol.RestartNode.MAGIC
 node = uavcan.make_node("can0", node_id=120)
 
 node.request(uavcan.protocol.RestartNode.request(), 20, response_callback)
+
+node.spin()
