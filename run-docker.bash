91528319ea624b8134f929eb5b9799590fa00459
ryan <4721319+rpshukla@users.noreply.github.com>
2019-09-11 09:15:47 -0600

    Set ROS_IP in docker run script and also set an environment variable for qt

--- run-docker.bash
+++ run-docker.bash
@@ -1,2 +1,2 @@
 # see: http://fabiorehm.com/blog/2014/09/11/running-gui-apps-with-docker/
-docker run -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:/root/.Xauthority --net=host spear
+docker run -it -e DISPLAY -e ROS_IP=127.0.0.1 -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:/root/.Xauthority --net=host spear
