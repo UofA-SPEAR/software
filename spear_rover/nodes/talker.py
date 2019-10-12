3e9b1cd6ed08997c9b3cbc715a377b3a6a52b277
MustafaFarow <farow@ualberta.ca>
2019-10-12 15:48:28 -0600

    Add simple publisher and subscriber

---
+++ spear_rover/nodes/talker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('my_topic', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        msg = "Current time: %s " % rospy.get_time()
+        msg += "Python"
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
