3aceace4206a51c1ca05b2cd705ddb69d83d2ffc
mdepp <mdepp@ualberta.ca>
2020-01-11 13:38:09 -0700

    Fix travis errors

--- spear_behaviors/spear_flexbe_states/setup.py
+++ spear_behaviors/spear_flexbe_states/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['spear_flexbe_states'],
-    package_dir = {'': 'src'}
+    packages=['spear_flexbe_states'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
