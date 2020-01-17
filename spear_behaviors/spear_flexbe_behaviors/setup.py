e15dc3b117d6f69221f80f753c1c65c0f2fbede0
mdepp <mdepp@ualberta.ca>
2020-01-17 12:47:24 -0700

    Update excluding spear_flexbe_behaviors in linting

--- spear_behaviors/spear_flexbe_behaviors/setup.py
+++ spear_behaviors/spear_flexbe_behaviors/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['spear_flexbe_behaviors'],
-    package_dir = {'': 'src'}
+    packages=['spear_flexbe_behaviors'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
