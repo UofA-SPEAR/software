23eec21627bd1de8dd7e16552012897a17386142
farow550 <farow@ualberta.ca>
2020-01-26 22:28:58 +0000

    The talker node to publish an array of values.

---
+++ spear_rover/MustafaNode/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vagrant/software/spear_rover/MustafaNode
+# Build directory: /home/vagrant/software/spear_rover/MustafaNode
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
