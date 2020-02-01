8aa7694f8782208567ffb003cdd3bb66e6c5a7fe
Johann Barnard <johann.b@telus.net>
2020-02-01 16:02:18 -0700

    Add improved scripts for setting up the CAN interface
    Developed after way too many hours of oscilloscope probing, googling,
    and generally being annoyed.
    
    Docs are being added to the wiki.
    
    Does not work from within docker yet - need to add the capability to the
    container through docker-compose.

--- spear_rover/vcan-setup.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Sets up a virtual CAN Bus interface on vcan0.
-
-echo "[WARNING]: We're using sudo to enable vcan! Sorry!"
-
-sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
