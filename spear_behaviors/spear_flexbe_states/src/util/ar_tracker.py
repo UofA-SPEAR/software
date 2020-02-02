7b94aae32b6e86a4ddd79a5188bdde69a9fe8f1c
mdepp <mdepp@ualberta.ca>
2020-02-01 17:47:27 -0700

    Fix some marker tracker issues

--- spear_behaviors/spear_flexbe_states/src/util/ar_tracker.py
+++ spear_behaviors/spear_flexbe_states/src/util/ar_tracker.py
@@ -1,19 +1,18 @@
 from typing import Dict, Optional
 
-from ar_track_alvar_msgs.msg import AlvarMarkers
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 from geometry_msgs.msg import PoseStamped
 from rospy import Subscriber
 
 
 class ARTracker:
-    def __init__(self, marker_topic, frame_id):
+    def __init__(self, marker_topic, frame_id):  # type: (str, str) -> None
         self._marker_poses = {}  # type: Dict[int, PoseStamped]
         self._frame_id = frame_id
         self._subscriber = Subscriber(marker_topic, AlvarMarkers, self.markers_callback, queue_size=1)
 
     def markers_callback(self, message):  # type: (AlvarMarkers) -> None
-        message = AlvarMarkers()
-        for marker in message.markers:
+        for marker in message.markers:  # type: AlvarMarker
             self._marker_poses[marker.id] = marker.pose
             # ar_track_alvar doesn't correctly add the pose frame id, so we need to add it here
             self._marker_poses[marker.id].header.frame_id = self._frame_id
