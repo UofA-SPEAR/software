3142f112a6f3745d8c04f80669790505b3f879b4
farow550 <farow@ualberta.ca>
2020-03-12 00:54:59 +0000

    Added moveit evan stuuf to moveit mustafa

--- unpack.sh
+++ unpack.sh
@@ -34,7 +34,7 @@ git submodule update --init --recursive
 cd ~
 mkdir -p ~/ros/src
 cd ~/ros
-catkin build
+catkin build -p 1
 # Add a line in .bashrc to source ros setup script
 # Uses grep to check if the line already exists
 if ! grep -Fxq "source ~/ros/devel/setup.bash" ~/.bashrc
@@ -108,6 +108,6 @@ ln -s ~/uavcan_dsdl/spear
 cd ~/ros
 # update rosdeps first
 rosdep install --from-paths src --ignore-src -r -y && \
-catkin build --force-cmake && \
+catkin build -p 1 --force-cmake && \
 
 printf "Thanks for unpacking!\nNow that your enviroment is setup, you should never have to do this again.\nPlease run the following command to install the correct packages:\nrosdep install --from-paths src --ignore-src -r -y\n"
