0bf56c71dbfefd0fc5917cba7fffdab412515e62
mdepp <mdepp@ualberta.ca>
2020-05-01 19:57:53 -0600

    Remove MustafaNode

--- spear_rover/MustafaNode/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vagrant/software/spear_rover/MustafaNode
-# Build directory: /home/vagrant/software/spear_rover/MustafaNode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
