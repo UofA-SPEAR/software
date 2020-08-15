e002c98935e887cabf6b121d9000edf54d86f202
Mitchell Epp <31737634+mdepp@users.noreply.github.com>
2020-08-15 14:04:18 -0600

    Cleanup some unused nodes and configs (#213)
    * Remove unused configs
    
    * Remove open-ez-publisher script
    
    Use rqt_joint_trajectory_controller (launched in steering) instead
    
    * Remove some more unused nodes

--- spear_rover/open-ez-publisher.sh
+++
@@ -1,4 +0,0 @@
-# Must apt install ros-kinetic-rqt-ez-publisher
-# roscore must be running
-# a node must be subscribed to the topic you're trying to publish to
-rosrun rqt_ez_publisher rqt_ez_publisher --slider-file config/arm_new.yaml --force-discover
