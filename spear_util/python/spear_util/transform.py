e1ab4c548487aa83df2ba5ff2ce8a562e8eda9d8
mdepp <mdepp@ualberta.ca>
2021-02-03 18:40:30 -0700

    Add some comments to transform.py

--- spear_util/python/spear_util/transform.py
+++ spear_util/python/spear_util/transform.py
@@ -25,6 +25,11 @@ def lookup_transform_simple(
         source_frame,
         tf_buffer=None
 ):  # type: (str, str, Optional[Buffer]) -> TransformStamped
+    """
+    A version of tf2_ros.TransformBuffer.lookup_transform which has some
+    reasonable defaults and is much simpler to use. The drawback is that this
+    function will hang forever if the transform cannot be performed.
+    """
     if tf_buffer is None:
         tf_buffer = get_tf_buffer()
     while not rospy.is_shutdown():
@@ -42,7 +47,15 @@ TransformableMessage = TypeVar("TransformableMessage", PointStamped,
                                Vector3Stamped, WrenchStamped, PoseStamped)
 
 
-def do_transform_msg(msg, target_frame, transform=None):
+def do_transform_msg(
+    msg,
+    target_frame,
+    transform=None
+):  # type: (Union[PointStamped, Vector3Stamped, WrenchStamped, PoseStamped], str, Optional[TransformStamped]) -> ...
+    """
+    Transforms a point, vector, wrench, or pose to the given frame. If no
+    transform is specified the correct transform is looked up automatically.
+    """
     if transform is None:
         transform = lookup_transform_simple(target_frame, msg.header.frame_id)
     if isinstance(msg, PointStamped):
@@ -59,8 +72,8 @@ def do_transform_msg(msg, target_frame, transform=None):
 def transform_to_matrix(
         transform):  # type: (Union[Transform, TransformStamped]) -> np.ndarray
     """
-    Converts a Transform message to the corresponding homogeneous transformation
-    matrix (an np array).
+    Converts a Transform message to the corresponding homogeneous (4x4)
+    transformation matrix (an np array).
     """
     if isinstance(transform, TransformStamped):
         transform = transform.transform
