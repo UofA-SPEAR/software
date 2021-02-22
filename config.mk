24fc96264f7239d22539f278648b7047b0add7b8
mdepp <mdepp@ualberta.ca>
2021-02-21 21:41:20 -0700

    Improve config comments and defaults

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ UNPACK_ENV=dev  # rover|dev
 
 # If true, nothing is built while unpacking (helpful if building the docker
 # image when the code contains a compile error).
-UNPACK_SKIP_BUILD=true  # true|false
+UNPACK_SKIP_BUILD=false  # true|false
 
 # If true, the build command copies headers and libs to .docker-*-mount/
 # directories where they can be seen outside of the docker container, and
@@ -14,7 +14,8 @@ UNPACK_SKIP_BUILD=true  # true|false
 # running your editor outside of the docker container.
 BUILD_COPY_HEADERS_AND_LIBS=false  # true|false
 
-# If true, the build command always performs dsdl generation as a prebuild step.
-# If false, this step is always skipped. I couldn't find a good way to encode
-# this as a proper dependency with Make.
+# If true, the build command always performs DSDL generation as a prebuild step.
+# If false, this step is always skipped. This is helpful if you will be changing
+# the DSDLs a lot and don't want to worry about running `make generate-dsdl`
+# every time, but is quite slow and so is disabled by default.
 BUILD_GENERATE_DSDL=false  # true|false
